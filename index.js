7e47f5ba8eaae54404994cc00338f7e9eed93469
Tumaini Muriithi <wendomuriithi@gmail.com>
2023-10-16 11:54:31 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Write your code here!
+let main = document.querySelector('#main');
+let head = document.createElement('h1');
+head.setAttribute("id","victory");
+
+head.innerHTML = `
+    <p>Hi my name is Michael de Santa and i am loving my retirement</p>
+    `
+
+main.append(head);
+
+
+// main.remove()
+
+
+
+
+
